108fc937bc7e51d2b6b37561ba77c1ec6e372db5
Connor-Kasper <66397543+Connor-Kasper@users.noreply.github.com>
2024-12-07 20:42:38 -0600

    Added Splitting apart

---
+++ SplitGameUnity/Assets/BigPlayer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BigPlayer : MonoBehaviour
+{
+    public GameObject notSplit;
+    public GameObject split1;
+    public GameObject split2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            split1.SetActive(true);
+            split1.transform.position = notSplit.transform.position;
+            split1.transform.position = new Vector3 (split1.transform.position.x - 0.5f, split1.transform.position.y, split1.transform.position.z) ;
+            split2.SetActive(true);
+            split2.transform.position = notSplit.transform.position;
+            split2.transform.position = new Vector3 (split2.transform.position.x + 0.5f, split2.transform.position.y, split2.transform.position.z);
+            notSplit.SetActive(false);
+        }
+    }
+}

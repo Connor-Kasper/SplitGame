61c03da685df7ed4820361426b73dded3dfa46f9
Connor-Kasper <66397543+Connor-Kasper@users.noreply.github.com>
2024-12-08 21:34:18 -0600

    Made Jumping :)

--- SplitGameUnity/Assets/Scripts/ControlManager.cs
+++ SplitGameUnity/Assets/Scripts/ControlManager.cs
@@ -4,20 +4,30 @@ public class ControlManager : MonoBehaviour
 {
     public GameObject curController;
     private Rigidbody2D rb;
+    public float jumpForce;
     public float speed;
     public GameObject split1;
     public GameObject split2;
+    public bool isGrounded;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float checkRadius = .2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = curController.transform.position;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+
         rb = curController.GetComponent<Rigidbody2D>();
+        transform.position = curController.transform.position;
+        
         if(curController == split1 && Input.GetKeyDown(KeyCode.E))
         {
             curController = split2;
@@ -25,10 +35,18 @@ public class ControlManager : MonoBehaviour
         {
             curController = split1;
         }
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+
     }
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
     }
+    
 }
